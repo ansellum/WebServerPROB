b9c3b78d25b147fe2474c9d1467011bb95841b14
ansellum <jbvogt@ucdavis.edu>
2023-03-07 18:23:49 -0800

    Added server setup

--- WebServerPROB.py
+++ WebServerPROB.py
@@ -9,6 +9,14 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # Fill in start
 
+# define local host & port to connect to
+HOST = "127.0.0.1"
+PORT = 6789
+
+# start socket server
+serverSocket.bind((HOST, PORT))
+serverSocket.listen()
+
 # Fill in end 
 
 # Server should be up and running and listening to the incoming connections
@@ -16,15 +24,15 @@ while True:
 	print('Ready to serve...')
 	
 	# Set up a new connection from the client
-	connectionSocket, addr = #Fill in start             #Fill in end
+	connectionSocket, addr = serverSocket.accept() # lots of documentation says this; dont understand myself but keeping for now
 	
 	# If an exception occurs during the execution of try clause
 	# the rest of the clause is skipped
 	# If the exception type matches the word after except
 	# the except clause is executed
 	try:
-		# Receives the request message from the client
-		message =  #Fill in start           #Fill in end
+		# Receives the request message (of max size 4096) from the client (& decodes into string)
+		message = connectionSocket.recv(4096).decode('utf-8')
 
 		# Extract the path of the requested object from the message
 		# The path is the second part of HTTP header, identified by [1]
@@ -34,7 +42,7 @@ while True:
 		# a character '\', we read the path from the second character 
 		f = open(filename[1:])
 
-		# Store the entire contenet of the requested file in a temporary buffer
+		# Store the entire content of the requested file in a temporary buffer
 		outputdata = #Fill in start         #Fill in end
 
 		# Send the HTTP response header line to the connection socket
