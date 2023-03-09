b3ff50c197e89c33ea44e783120e21fa4a3ff8dc
ansellum <jbvogt@ucdavis.edu>
2023-03-09 13:12:45 -0800

    Completed first draft

--- ProxyServer.py
+++ ProxyServer.py
@@ -24,7 +24,7 @@ while True:
 	print('Ready to serve...')
 	
 	# Set up a new connection from the client
-	connectionSocket, addr = serverSocket.accept() # lots of documentation says this; dont understand myself but keeping for now
+	client_sock, client_addr = serverSocket.accept() # lots of documentation says this; dont understand myself but keeping for now
 	
 	# If an exception occurs during the execution of try clause
 	# the rest of the clause is skipped
@@ -32,35 +32,47 @@ while True:
 	# the except clause is executed
 	try:
 		# Receives the request message (of max size 4096) from the client (& decodes into string)
-		message = connectionSocket.recv(4096).decode() # Custom Code
+		message = client_sock.recv(4096).decode() # Custom Code
 
 		# message looks like 
 		# 	GET /localhost:6789/helloworld.html HTTP/1.1
 		# 	...
-		request = message.split()[1]		# /localhost:6789/helloworld.html
-		
-		serverAddr = request.split('/')[1] 	# localhost:6789
-		f = request.split('/')[2]			# helloworld.html
+		request = message.split()[1]			# /localhost:6789/helloworld.html
 
-		# Send the HTTP response header line to the connection socket
-		# Fill in start
+		destination = request.split('/')[1] 	# localhost:6789
+		dest_addr = destination.split(':')[0]	# localhost
+		dest_port = destination.split(':')[1]	# 6789
 
-		# Fill in end
+		# Connect to the destination server
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+		s.connect((dest_addr, dest_port))
  
-		# Send the content of the requested file to the connection socket
+		# Send the content of the requested file to destination server
+		s.sendall(request)
+
+		# Receive destination server response and send it to client
+		while True:
+			# receive data from web server
+			data = s.recv(4096)
+
+			# Keep sending until no more data
+			if (len(data) > 0):
+				client_sock.send(data) # send to browser/client
+			else:
+				break
 		
 		# Close the client connection socket
-		connectionSocket.close()
+		client_sock.close()
 
 	except IOError:
 		# Send HTTP response message for file not found
 		# Fill in start
-		connectionSocket.send("404 Not Found".encode())
+		client_sock.send("404 Not Found".encode())
 		# Fill in end
         
 		# Close the client connection socket
 		# Fill in start
-		connectionSocket.close()
+		client_sock.close()
 		# Fill in end
 
 serverSocket.close()  
